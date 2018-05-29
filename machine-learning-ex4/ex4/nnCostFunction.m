a56007531ba505024230f7233e7c073efda7351a
Fernando Narbona <fnarbona@tracfone.com>
2018-05-29 15:55:05 -0400

    ex4 complete

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -34,51 +34,34 @@ Theta2_grad = zeros(size(Theta2));
 % Instructions: You should complete the code by working through the
 %               following parts.
 %
-% Part 1: Feedforward the neural network and return the cost in the
-%         variable J. After implementing Part 1, you can verify that your
-%         cost function computation is correct by verifying the cost
-%         computed in ex4.m
-%
-% Part 2: Implement the backpropagation algorithm to compute the gradients
-%         Theta1_grad and Theta2_grad. You should return the partial derivatives of
-%         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
-%         Theta2_grad, respectively. After implementing Part 2, you can check
-%         that your implementation is correct by running checkNNGradients
-%
-%         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
-%               binary vector of 1's and 0's to be used with the neural network
-%               cost function.
-%
-%         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
-%               first time.
-%
+% Part 1: Feedforward the neural network and return the cost in the variable J.
+
+yy = zeros(size(y),num_labels);
+for i = 1:size(yy,1)
+  yy(i,y(i)) = 1;
+endfor
+
+a1 = [ones(m, 1) X];
+a2 = [ones(m, 1) sigmoid(a1 * Theta1')];
+a3 = sigmoid(a2 * Theta2');
+hx = a3;
+
+J = (1/m) * sum(sum(-yy.*log(hx) - (1-yy).*log(1-hx)));
+reg = (lambda/(2*m)) * (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
+J = J + reg;
+
+% Part 2: Implement the backpropagation algorithm to compute the gradients Theta1_grad and Theta2_grad.
+
+d3 = a3 - yy;
+d2 = (d3*Theta2).*[ones(size((a1 * Theta1'),1),1) sigmoidGradient(a1 * Theta1')];
+
+Theta2_grad = Theta2_grad + (1/m)*(d3'*a2);
+Theta1_grad = Theta1_grad + (1/m)*(d2(:,2:end)'*a1);
+
 % Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-%
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m)*(Theta1(:,2:end));
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda/m)*(Theta2(:,2:end));
 
 % -------------------------------------------------------------
 
